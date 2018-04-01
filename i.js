f0973adc122e943c65a4f1dbab5d73e220082ed7
hyn <yanannewmail@yeah.net>
2018-04-01 16:08:10 +0800

    aaa

--- i.js
+++ i.js
@@ -1,13 +1,7 @@
 window.test = function (num) {
-    if (num == 1){console.log(num);
-        return 1;devdevdevdev
-    } else{console.log(555);
-        return num + 1;devdevdevdev
-		mastermastermasster
+    if (num == 1){
+        return 1;
+    } else{
+        return num + 1;
     }
 }
-11111111dev1
-2222222222222dev
-8888888888888dev
-kkkkkkkkkkkkkkkkmaster
-uuuuuuuuuuuuuuuaaaaaaaaaaaaaaa
