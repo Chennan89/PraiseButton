c7df0261f77a4ec6fa689a48a36c9a72e2e82ea4
hyn <yanannewmail@yeah.net>
2018-04-01 14:10:04 +0800

    df

--- i.js
+++ i.js
@@ -1,8 +1,8 @@
 window.test = function (num) {
-    if (num == 1) {
-        lslkdf
-		return 1
-    } else {
+	aaaaaaaaaaaaaaa
+    if (num == 1){console.log(num);
+        return 1;
+    } else{console.log(555);
         return num + 1;
     }
 }
